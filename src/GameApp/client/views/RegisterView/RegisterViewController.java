902cf11f8eab876ae00b2fdbc584ed64cc14883f
Andreea101018 <andreea.asimine@yahoo.com>
2022-11-30 17:32:29 +0100

    30.11.2022

--- src/GameApp/client/views/RegisterView/RegisterViewController.java
+++ src/GameApp/client/views/RegisterView/RegisterViewController.java
@@ -2,13 +2,12 @@ package GameApp.client.views.RegisterView;
 
 import GameApp.client.core.ViewHandler;
 import GameApp.client.core.ViewModelFactory;
-import GameApp.client.views.LoginView.LoginViewModel;
 import GameApp.client.views.ViewController;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import java.rmi.RemoteException;
 import java.sql.SQLException;
 
 public class RegisterViewController implements ViewController
@@ -16,8 +15,11 @@ public class RegisterViewController implements ViewController
   private RegisterViewModel registerViewModel;
   private ViewHandler vh;
   @FXML
-  private TextField emailField, addressField, confirmAddressField, passwordField, confirmPasswordField, usernameField;
+  private TextField emailField, confirmEmailField, countryField, usernameField, addressField, passwordField, confirmPasswordField;
   @FXML Label errorLabel;
+  @FXML
+  CheckBox notRobotCheckBox, yearsCheckBox;
+
 
 
   @Override public void init(ViewHandler vh, ViewModelFactory vmf)
@@ -26,22 +28,37 @@ public class RegisterViewController implements ViewController
     this.registerViewModel = vmf.getRegisterViewModel();
 
     emailField.textProperty().bindBidirectional(registerViewModel.emailProperty());
+    confirmEmailField.textProperty().bindBidirectional(registerViewModel.confirmEmailProperty());
+    countryField.textProperty().bindBidirectional(registerViewModel.countryProperty());
     addressField.textProperty().bindBidirectional(registerViewModel.addressProperty());
-    confirmAddressField.textProperty().bindBidirectional(registerViewModel.confirmAddressProperty());
+    usernameField.textProperty().bindBidirectional(registerViewModel.usernameProperty());
     passwordField.textProperty().bindBidirectional(registerViewModel.passwordProperty());
     confirmPasswordField.textProperty().bindBidirectional(registerViewModel.confirmPasswordProperty());
-    usernameField.textProperty().bindBidirectional(registerViewModel.usernameProperty());
-    errorLabel.textProperty().bind(registerViewModel.errorProperty());
   }
 
   @FXML
   private void backToLogin() {
     vh.openLoginView();
+    emailField.clear();
+    confirmEmailField.clear();
+    countryField.clear();
+    addressField.clear();
+    usernameField.clear();
+    passwordField.clear();
+    confirmPasswordField.clear();
+    notRobotCheckBox.selectedProperty().setValue(false);
+    yearsCheckBox.selectedProperty().setValue(false);
+    errorLabel.textProperty().set("");
   }
 
   @FXML
   private void saveInfo() throws SQLException {
-    registerViewModel.addUser();
+    if(notRobotCheckBox.isSelected() && yearsCheckBox.isSelected()) {
+      errorLabel.textProperty().bindBidirectional(registerViewModel.errorProperty());
+      registerViewModel.addUser();
+    }
+    else
+    {errorLabel.textProperty().setValue(("CheckBoxes ERROR!"));}
   }
 
 
--- src/GameApp/client/views/RegisterView/RegisterViewModel.java
+++ src/GameApp/client/views/RegisterView/RegisterViewModel.java
@@ -3,20 +3,25 @@ package GameApp.client.views.RegisterView;
 import GameApp.client.model.ClientModelManagerFactory;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.scene.control.SplitMenuButton;
 
+import java.awt.*;
 import java.sql.SQLException;
 
 public class RegisterViewModel
 {
   private ClientModelManagerFactory clientModelManagerFactory;
-  private StringProperty email, address, confirmAddress, username, password, confirmPassword, error;
+  private StringProperty email, confirmEmail, country, address,username, password, confirmPassword, error;
+
+
 
   public RegisterViewModel(ClientModelManagerFactory clientModelManagerFactory)
   {
     this.clientModelManagerFactory = clientModelManagerFactory;
     email = new SimpleStringProperty();
+    confirmEmail = new SimpleStringProperty();
+    country = new SimpleStringProperty();
     address = new SimpleStringProperty();
-    confirmAddress = new SimpleStringProperty();
     username = new SimpleStringProperty();
     password = new SimpleStringProperty();
     confirmPassword = new SimpleStringProperty();
@@ -26,38 +31,49 @@ public class RegisterViewModel
   public void addUser() throws SQLException {
     error.set("");
     String input1 = email.get();
-    String input2 = address.get();
-    String input3 = confirmAddress.get();
-    String input4 = username.get();
-    String input5 = password.get();
-    String input6 = confirmPassword.get();
+    String input2 = confirmEmail.get();
+    String input3 = country.get();
+    String input4 = address.get();
+    String input5 = username.get();
+    String input6 = password.get();
+    String input7 = confirmPassword.get();
 
-    if(input1 != null && !"".equals(input1) && input2 != null && !"".equals(input2) && input3 != null && !"".equals(input3)
-    && input4 != null && !"".equals(input4) && input5 != null && !"".equals(input5) && input6 != null && !"".equals(input6)) {
-      if(checkEmail(input1))
-      {
-        error.set("Email already used in the system! Please introduce another email");
-      }
-      else
-      {
-        clientModelManagerFactory.addUser(input1, input2, input4, input4, input5);
-        error.set("Account saved!");
-      }
+    if (input1 != null && !"".equals(input1) && input2 != null && !"".equals(input2) && input3 != null && !"".equals(input3)
+            && input4 != null && !"".equals(input4) && input5 != null && !"".equals(input5) && input6 != null && !"".equals(input6)
+            && !"".equals(input5) && input6 != null && !"".equals(input6) && input7 != null && !"".equals(input7)) {
+      if (!input1.equals(input2)) {
+        error.set("The emails are not matching!");
+      } else {
+        if (checkEmail(input1)) {
+          error.set("Email already used in the system! Please introduce another email");
+        } else {
+          if (input6.equals(input7)) {
+            clientModelManagerFactory.addUser(input1, input2, input4, input4, input5);
+            error.set("Account saved!");
 
-    } else {
-      error.set("ERROR: Input cannot be empty");
+        } else
+        error.set("The passwords are not matching!");
+      }
     }
+      }
+       else
+        error.set("ERROR: Input cannot be empty");
   }
 
 
   public StringProperty emailProperty() {
     return email;
   }
+  public StringProperty confirmEmailProperty()
+  {
+    return confirmEmail;
+  }
+  public StringProperty countryProperty()
+  {
+    return country;
+  }
   public StringProperty addressProperty() {
     return address;
-  }
-  public StringProperty confirmAddressProperty() {
-    return confirmAddress;
   }
   public StringProperty usernameProperty() {
     return username;
