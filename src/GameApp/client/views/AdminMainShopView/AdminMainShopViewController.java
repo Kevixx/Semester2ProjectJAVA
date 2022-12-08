3040a1f09f4877c9f8390cd9e77a9266d130e8a2
Andreea101018 <andreea.asimine@yahoo.com>
2022-12-08 17:26:53 +0100

    Andreea 08.12.2022

--- src/GameApp/client/views/AdminMainShopView/AdminMainShopViewController.java
+++ src/GameApp/client/views/AdminMainShopView/AdminMainShopViewController.java
@@ -5,14 +5,24 @@ import GameApp.client.core.ViewModelFactory;
 import GameApp.client.views.ViewController;
 import javafx.fxml.FXML;
 
+import javax.swing.text.html.ListView;
+import java.sql.SQLException;
+
 public class AdminMainShopViewController implements ViewController {
     private AdminMainShopViewModel adminMainShopViewModel;
     private ViewHandler vh;
-
+    @FXML
+    ListView gameList;
 
     @Override
     public void init(ViewHandler vh, ViewModelFactory vmf) {
         this.adminMainShopViewModel = vmf.getAdminMainShopViewModel();
+       gameList = getGameList();
+    }
+
+    @FXML
+    public ListView getGameList() {
+        return adminMainShopViewModel.getGameList();
     }
    @FXML
    private void myAccount() {vh.openMyAccountView();}
