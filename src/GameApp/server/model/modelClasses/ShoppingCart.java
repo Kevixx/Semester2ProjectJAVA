754a1e71bdd9a356ee85f0bdca67f690504a9a61
Slardarz <WARpoks29>
2022-12-11 13:42:06 +0100

    ShoppingCart class changes

--- src/GameApp/server/model/modelClasses/ShoppingCart.java
+++ src/GameApp/server/model/modelClasses/ShoppingCart.java
@@ -6,10 +6,25 @@ public class ShoppingCart{
 
 	private ArrayList<Game> games;
 
+	public ShoppingCart()
+	{
+		games = new ArrayList<>();
+	}
+
 	public void addGame(int GameId) {
 	}
 
 	public void removeGame(int GameId) {
+	}
+
+	public double getShoppingCartvalue()
+	{
+		double value = 0;
+		for (int i = 0; i < games.size(); i++)
+		{
+			value+= games.get(i).getGamePrice();
+		}
+		return value;
 	}
 
 }
